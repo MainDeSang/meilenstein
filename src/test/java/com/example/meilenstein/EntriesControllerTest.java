03abd88c4b315cabd3f33163ac1fda854dcb1e69
johann <johann.lindele@tn.mischok.academy>
2025-07-01 11:10:45 +0200

    first

--- src/test/java/com/example/meilenstein/EntriesControllerTest.java
+++ src/test/java/com/example/meilenstein/EntriesControllerTest.java
@@ -4,9 +4,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,10 +27,28 @@ public class EntriesControllerTest {
 
     @Test
     public void testControllerPerson() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/person"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+        mockMvc.perform(get("/person"))
+                .andExpect(status().isOk());
 
     }
 
+    @Test
+    public void testEntriesGetIsAviableAndReturnData() throws Exception {
+        mockMvc.perform(get("/person"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Meilenstein"))
+                .andExpect(jsonPath("$", hasSize(3)))
+
+                .andExpect(jsonPath("$[0].firstName").isNotEmpty())
+                .andExpect(jsonPath("$[1].lastName").isNotEmpty())
+                .andExpect(jsonPath("$[2].dateOfBirth").isNotEmpty())
+
+                .andExpect(jsonPath("$[0].firstName", is("Max")))
+                .andExpect(jsonPath("$[1].lastName", is("Mustermann")))
+                .andExpect(jsonPath("$[2].dateOfBirth", is(1997 - 06 - 23)));
+
+
     }
+
+}
 
