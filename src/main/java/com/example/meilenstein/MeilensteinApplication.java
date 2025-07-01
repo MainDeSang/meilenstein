d4d2a6fce5fa493e58f03fffd9de95222204c654
johann <johann.lindele@tn.mischok.academy>
2025-07-01 09:54:54 +0200

    Initial commit

---
+++ src/main/java/com/example/meilenstein/MeilensteinApplication.java
@@ -0,0 +1,13 @@
+package com.example.meilenstein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeilensteinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeilensteinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/meilenstein/MeilensteinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meilenstein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeilensteinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
